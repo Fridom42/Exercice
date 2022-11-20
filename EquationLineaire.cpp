ea1e844b7727922b53c6507e4d3636cb29e50b5e
reyane redjem <Fridom42@outlook.fr>
2022-11-20 22:23:55 +0100

    ajout plein de fonction

--- EquationLineaire.cpp
+++ EquationLineaire.cpp
@@ -1,10 +1,103 @@
 #include "EquationLineaire.hpp"
 
 
-EquationLineaire::EquationLineaire(/* args */)
+EquationLineaire::EquationLineaire()
 {
+    nRows = 4;
+    nCols = 4;
+
+    SetA(nRows,nCols);
+    Set_b(nRows);
+}
+EquationLineaire::EquationLineaire(int Rows, int Cols)
+{
+    nRows = Rows;
+    nCols = Cols;
+
+    SetA(nRows,nCols);
+    Set_b(nRows);
 }
 
 EquationLineaire::~EquationLineaire()
 {
 }
+
+vector <vector<int>> EquationLineaire::GetA()
+{
+    return MatriceA;
+}
+void EquationLineaire::SetA(int Rows, int Cols)
+{
+    cout << "Saisisser les valeurs de votre matrice:" << endl;
+    for (int col = 0; col < nCols; col++)
+    {
+        for (int row = 0; row < nRows; row++)
+        {
+            cout << "A" << row << '.' << col << "= ";
+            cin >> MatriceA[row][col];
+        }
+        
+    }
+}
+vector<int> EquationLineaire::Get_b()
+{
+    return Vecteur_b;
+}
+void EquationLineaire::Set_b()
+{
+    cout << "Saisisser les valeurs de votre vecteur:" << endl;
+    for (int row = 0; row < nRows; row++)
+    {
+        cout << "b" << row << "= ";
+        cin >> Vecteur_b[row];
+    }
+}
+
+bool EquationLineaire::IsSquared(vector<vector<int>> Matrice)
+{
+    if (Matrice[0].size() == Matrice.size()) {return true;}
+    else {return false;}
+}
+bool EquationLineaire::IsUpperTriangular(vector<vector<int>> Matrice)
+{
+    if(!IsSquared(Matrice)) {return false;}
+
+    for (int i = 1; i < Matrice.size(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (Matrice[i][j] != 0) { return false; }
+        }
+    }
+    return true;
+}
+bool EquationLineaire::IsLowerTriangular(vector<vector<int>> Matrice)
+{
+    if(!IsSquared(Matrice)) {return false;}
+
+    for (int i = 0; i < Matrice.size(); i++)
+    {
+        for (int j = i+1; j < Matrice.size(); j++)
+        {
+            if (Matrice[i][j] != 0) { return false; }
+        }
+    }
+    return true;
+}
+
+int EquationLineaire::determinant(vector <vector <int>> Matrice)
+{
+
+}
+
+vector<vector<int>> EquationLineaire::Triangularise(vector<vector<int>> Matrice)
+{
+
+}
+
+vector <int> EquationLineaire::solver(vector<vector <int>> Matrice, vector <int> Vecteur)
+{
+
+}
+
+
--- EquationLineaire.hpp
+++ EquationLineaire.hpp
@@ -2,21 +2,37 @@
 #define EQUATIONLINEAIRE_H
 
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class EquationLineaire
 {
-private:
-    int nRows;
-    int nCols;
-    vector <vector <int>> MatriceA;
-    vector <int> b;
-    vector <int> x;
-    
-public:
-    EquationLineaire();
-    ~EquationLineaire();
+    private:
+        int nRows;
+        int nCols;
+        vector <vector <int>> MatriceA;
+        vector <int> Vecteur_b;
+        vector <int> Vecteur_x;
+        
+    public:
+        EquationLineaire();
+        EquationLineaire(int Rows, int Cols);
+        ~EquationLineaire();
 
+        vector <int> solver(vector<vector <int>> Matrice, vector <int> Vecteur);
+    
+        int determinant(vector <vector <int>> Matrice);
+        
+        vector <vector<int>> GetA();
+        void SetA(int Rows, int Cols);
+        vector<int> Get_b();
+        void Set_b();
+    
+        bool IsSquared(vector<vector<int>> Matrice);
+        bool IsUpperTriangular(vector<vector<int>> Matrice);
+        bool IsLowerTriangular(vector<vector<int>> Matrice);
+
+        vector<vector<int>> Triangularise(vector<vector<int>> Matrice);
 };
 #endif
